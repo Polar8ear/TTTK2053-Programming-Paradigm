d84b8e2d45b45868acf8b6091d893c05782133af
Chuo <ignatius.chuo@seamlessx.com>
2023-04-05 09:38:49 +0800

    feat: complete lab1

---
+++ lab1/A187806.py
@@ -0,0 +1,22 @@
+def writeFile(filename):
+    with open(filename, 'w') as f:
+        f.writelines([
+            'A187806\n',
+            'Software Technology\n',
+        ])
+
+
+def readFile(filename):
+    with open(filename, 'r') as f:
+        content = f.read()
+        print(content*3)
+
+
+def main():
+    filename = 'python_file.txt'
+    writeFile(filename)
+    readFile(filename)
+
+
+if __name__ == '__main__':
+    main()
